bafc0c8aee93e5e094aaa8962a745483eb5a8dd1
Vitaliy Ognev <vitaliy.ognev@gmail.com>
2017-04-02 13:21:08 +0300

    templates

--- elliptics/entrypoint.sh
+++ elliptics/entrypoint.sh
@@ -4,8 +4,19 @@ set -e
 
 mkdir -p /var/lib/elliptics/{history,blobdata}
 
+PUBLIC_IP=`ip route get 8.8.8.8 | grep dev | awk '{print $7}'`
+REMOTE_IP=${REMOTE_IP:-autodiscovery:224.0.0.5}
+AUTH_COOKIE=${AUTH_COOKIE:-elliptics}
+
 CMD="$@"
 if [ -z "$CMD" ]; then
+
+  sed \
+    -e "s/{PUBLIC_IP}/$PUBLIC_IP/" \
+    -e "s/{REMOTE_IP}/$REMOTE_IP/" \
+    -e "s/{AUTH_COOKIE}/$AUTH_COOKIE/" \
+    -i /etc/ioserv.json
+
   CMD="$CMD /usr/bin/dnet_ioserv"
   CMD="$CMD -c /etc/ioserv.json"
 fi
