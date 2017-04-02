096beab4f7658caa236e35628af1a853605348db
Vitaliy Ognev <vitaliy.ognev@gmail.com>
2017-04-02 14:42:50 +0300

    PUBLIC_IP returns

--- elliptics/entrypoint.sh
+++ elliptics/entrypoint.sh
@@ -4,10 +4,13 @@ set -e
 
 mkdir -p /var/lib/elliptics/{history,blobdata}
 
+PUBLIC_IP=$(cat /etc/hosts | grep "${HOSTNAME}" | awk '{ print $1 }')
 REMOTE_IP=${REMOTE_IP:-autodiscovery:224.0.0.5}
 AUTH_COOKIE=${AUTH_COOKIE:-elliptics}
 REMOTE_PORT=${REMOTE_PORT:-1025}
+
 sed \
+    -e "s/{PUBLIC_IP}/$PUBLIC_IP/" \
     -e "s/{REMOTE_IP}/$REMOTE_IP/" \
     -e "s/{REMOTE_PORT}/$REMOTE_PORT/" \
     -e "s/{AUTH_COOKIE}/$AUTH_COOKIE/" \
